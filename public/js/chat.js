e2076d8aec510d84332493a171f4f4f9545b2937
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2020-06-20 14:57:46 +0530

    Tesing Basic Socketio Functionalities

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,12 @@
-io()
+const socket = io()
+const incrementButton = document.querySelector('#increment-button')
+
+incrementButton.addEventListener('click', ()=>{
+    console.log('clicked')
+    socket.emit('increment')
+})
+
+socket.on('countUpdated', (count)=>{
+    console.log('Count Had Been Updated', count);
+    
+})
