efca206962a8c8297deb7f646b6f6add847eabd1
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2020-06-20 14:33:34 +0530

    Socketio Instantiated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 var app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
+
+io.on('connection', ()=>{
+    console.log('New Web Socket Connection');
+    
+})
 
 server.listen(PORT, ()=>{
     console.log('Hi, Running on Port : ' + PORT);
