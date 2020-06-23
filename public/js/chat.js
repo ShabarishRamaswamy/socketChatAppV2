4c58a836d35903ac1b374f1ad1d5f9896cbed6bc
ShabarishRamaswamy <shabarishramaswamy@gmail.com>
2020-06-23 18:59:36 +0530

    Added accidental-spam prevention

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,27 @@
 const socket = io()
 
+//Elements 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.getElementById('send-location')
+
 socket.on('message', (msg)=>{
     console.log(msg)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e)=>{
+$messageForm.addEventListener('submit', (e)=>{
     e.preventDefault()
+    //Disable the submit button
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message, (error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        // Enable
         if(error){
             return console.log(error)
         }
@@ -22,10 +35,12 @@ document.querySelector('#send-location').addEventListener('click', ()=>{
     }
 
     navigator.geolocation.getCurrentPosition((position)=>{
+        $sendLocationButton.setAttribute('disabled', 'disabled')
         socket.emit('userLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, (error)=>{
+            $sendLocationButton.removeAttribute('disabled')
             if(error){
                 return console.log(error)
             }
